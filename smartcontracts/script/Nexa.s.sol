dda3314591e3ad9857590998630ad3b4317a0d01
Marcos David <david.jc7@gmail.com>
2024-03-22 16:06:48 -0300

    fit: create nexa script

--- smartcontracts/script/Nexa.s.sol
+++ smartcontracts/script/Nexa.s.sol
@@ -2,11 +2,30 @@
 pragma solidity ^0.8.13;
 
 import {Script, console} from "forge-std/Script.sol";
+import {NEXA} from "../src/Nexa.sol";
 
 contract NexaScript is Script {
+    NEXA nexa;
+
     function setUp() public {}
 
     function run() public {
-        vm.broadcast();
+        vm.startBroadcast(0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80);
+        //Deploy do contrato
+
+        nexa = new NEXA();
+
+        //Pegar o enderço do contraro
+        console.log("ADDRESS: ", address(nexa));
+
+        console.log("OWNER", nexa.owner());
+
+        vm.stopBroadcast();
     }
 }
+
+/* Rodar Script
+
+forge script script/Nexa.s.sol:NexaScript --rpc-url "http://127.0.0.1:8545" --broadcast
+
+*/
